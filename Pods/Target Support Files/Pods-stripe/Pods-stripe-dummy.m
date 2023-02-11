c9bda5ea3b338ea7918f059aca150b9563e94904
Miguel Faggioni <miguelfaggioni@gmail.com>
2023-02-11 12:33:08 -0400

    delete pods

--- Pods/Target Support Files/Pods-stripe/Pods-stripe-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_stripe : NSObject
-@end
-@implementation PodsDummy_Pods_stripe
-@end
--- Pods/Target Support Files/Pods-stripeTests/Pods-stripeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_stripeTests : NSObject
-@end
-@implementation PodsDummy_Pods_stripeTests
-@end
